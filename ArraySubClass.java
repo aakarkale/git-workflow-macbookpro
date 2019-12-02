9d94de08ad241f40235dc224d34f39898111359b
Aakar Kale <aakarkale@gmail.com>
2019-12-02 00:49:15 -0800

    Added ArraySubClass.java

---
+++ ArraySubClass.java
@@ -0,0 +1,16 @@
+/*
+Inheritance can be defined as the process where one class acquires the properties (methods and fields) of another.
+With the use of inheritance the information is made manageable in a hierarchical order.
+
+So Basically a SubClass inherits everything except for the Constructor.
+ */
+
+
+public class ArraySubClass
+{
+    public static void main(String[] args)
+    {
+        ArraySuperClass obj = new ArraySuperClass();
+        obj.displayArray();
+    }
+}
